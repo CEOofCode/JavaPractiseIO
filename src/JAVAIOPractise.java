7315637b75246e10bd47ffe797e23d9d91b7ae61
CEOofCode <owenfleming88@gmail.com>
2024-02-14 11:56:57 +0000

    input for colour

--- src/JAVAIOPractise.java
+++ src/JAVAIOPractise.java
@@ -1,10 +1,24 @@
+import java.util.Scanner; //Needed for input
 
 public class JAVAIOPractise {
 	
 	
 	
 	public static void main(String[] args) {
+		//prints out hello world
 		System.out.println("Hello World!");
+		
+		//variable used for output
+		String toPrint = "What Is Your Name Favourite Colour?";
+		
+		System.out.println(toPrint);
+		
+		//input Example
+		Scanner input = new Scanner(System.in);
+		
+		String colourChoice = input.nextLine();
+		
+		System.out.println("My Favourite Colour is " + colourChoice);
 	}
 	
 	
